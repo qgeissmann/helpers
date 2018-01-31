96792b21a135a94df07b17469c8e9233f04bff9c
Quentin Geissmann <qgeissmann@gmail.com>
2018-01-31 21:35:42 +0000

    add command to play bbc4 from command line

--- aliases.sh
+++ aliases.sh
@@ -7,14 +7,16 @@ alias cdd='cd ~/Desktop'
 alias cdt='cd /tmp'
 alias cdc='cd ~/comput/'
 alias cdo='cd ~/ownCloud/'
-alias cdr='cd ~/comput/rethomics-git'
+alias cdr='cd ~/comput/rethomics/'
 alias cde='cd ~/comput/ethoscope-git'
 alias x='xdg-open'
 alias pmake='time nice make -j$NUMCPUS --load-average=$NUMCPUS'
 alias yuy='echo "Running yaourt -Syua" && yaourt -Syua'
 alias grep='grep --color=auto'
 alias mplayer='mplayer -fs'
-
+alias switch_gpu='sudo sh ~/comput/helpers-git/switch_gpu.sh'
+#see curseradio to gegt the url
+alias bbc4='mpv http://open.live.bbc.co.uk/mediaselector/5/select/version/2.0/mediaset/http-icy-mp3-a/vpid/bbc_radio_fourlw/format/pls.pls'
 echo " "
 echo "==========================="
 echo 'Your aliases:'
