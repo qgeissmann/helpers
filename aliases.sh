654186a207a3e95f5fcb8557286c2a7eda0fdcca
quentin geissmann <qgeissmann@gmail.com>
2018-04-06 21:04:53 +0100

    ibid

--- aliases.sh
+++ aliases.sh
@@ -9,6 +9,8 @@ alias cdc='cd ~/comput/'
 alias cdo='cd ~/ownCloud/'
 alias cdr='cd ~/comput/rethomics/'
 alias cde='cd ~/comput/ethoscope-git'
+alias cdh='cd ~/comput/helpers-git'
+alias cdp='cd ~/comput/ImperialPhD/PhD/phd_thesis'
 alias x='xdg-open'
 alias pmake='time nice make -j$NUMCPUS --load-average=$NUMCPUS'
 alias yuy='echo "Running yaourt -Syua" && yaourt -Syua'
@@ -17,6 +19,8 @@ alias mplayer='mplayer -fs'
 alias switch_gpu='sudo sh ~/comput/helpers-git/switch_gpu.sh'
 #see curseradio to gegt the url
 alias bbc4='mpv http://open.live.bbc.co.uk/mediaselector/5/select/version/2.0/mediaset/http-icy-mp3-a/vpid/bbc_radio_fourlw/format/pls.pls'
+
+alias defselect='gnome-terminal -e "$(sdcv $(xclip -o -sec) -n -0)"'
 echo " "
 echo "==========================="
 echo 'Your aliases:'
